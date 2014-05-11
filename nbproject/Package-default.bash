5041776f4ea60fe97398c0b465a315b7084c5d1e
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-05-11 22:30:53 +0200

    Project update

--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/vacuum.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=vacuum.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=vacuum.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/vacuum.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=vacuum.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=vacuum/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/vacuum.x/bin
+makeDirectory ${TMPDIR}/vacuum/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/vacuum.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/vacuum.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/vacuum.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/vacuum.tar *
 checkReturnCode
 
 # Cleanup
