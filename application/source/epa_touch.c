b9f2d6cacd7371514812e2473816459cea78f491
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-05-15 12:16:01 +0200

    Touch EPA template is done

--- application/source/epa_touch.c
+++ application/source/epa_touch.c
@@ -17,3 +17,39 @@ enum touchStateId {
 struct wspace {
 
 };
+
+/*=============================================  LOCAL FUNCTION PROTOTYPES  ==*/
+
+static esAction stateInit               (struct wspace *, const esEvent *);
+
+/*=======================================================  LOCAL VARIABLES  ==*/
+
+static const ES_MODULE_INFO_CREATE("Touch interface", CONFIG_EPA_TOUCH_NAME, "Nenad Radulovic");
+
+static const esSmTable  TouchTable[] = ES_STATE_TABLE_INIT(TOUCH_TABLE);
+
+/*======================================================  GLOBAL VARIABLES  ==*/
+
+const struct esEpaDefine TouchEpa = ES_EPA_DEFINE(
+    CONFIG_EPA_TOUCH_NAME,
+    CONFIG_EPA_TOUCH_PRIORITY,
+    CONFIG_EPA_TOUCH_QUEUE_SIZE);
+const struct esSmDefine  TouchSm = ES_SM_DEFINE(
+    TouchTable,
+    sizeof(struct wspace),
+    stateInit);
+struct esEpa *           Touch;
+
+/*============================================  LOCAL FUNCTION DEFINITIONS  ==*/
+
+static esAction stateInit(struct wspace * space, const esEvent * event) {
+    
+    return (ES_STATE_IGNORED());
+}
+
+/*===================================  GLOBAL PRIVATE FUNCTION DEFINITIONS  ==*/
+/*====================================  GLOBAL PUBLIC FUNCTION DEFINITIONS  ==*/
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//******************************************************
+ * END of epa_touch.c
+ ******************************************************************************/
