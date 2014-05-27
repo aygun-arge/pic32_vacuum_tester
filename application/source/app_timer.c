274a328b2bc65f7835043cf5d718e82405801a7d
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-05-27 23:44:15 +0200

    Timer is a bit fortified

--- application/source/app_timer.c
+++ application/source/app_timer.c
@@ -40,6 +40,7 @@ void appTimerStart(
     esSysTimerTick      tick,
     uint16_t            eventId) {
 
+    esVTimerCancel(&timer->timer);
     timer->eventId = eventId;
     esVTimerStart(&timer->timer, tick, timeout, timer);
 }
