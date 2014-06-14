f51bde4da8e464bf75052cf7f6ab30d80bf5b923
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-06-14 23:00:26 +0200

    Added more methods

--- application/source/app_user.c
+++ application/source/app_user.c
@@ -1,3 +1,21 @@
 
 #include "app_user.h"
 
+
+static const char * const GlobalUserName[] = {
+    "administrator",
+    "operator"
+};
+
+static uint32_t GlobalUserId;
+
+void appUserSetCurrent(uint32_t id) {
+    GlobalUserId = id;
+}
+
+void appUserGetCurrent(struct appUser * user) {
+    user->id   = GlobalUserId;
+    user->name = GlobalUserName[GlobalUserId];
+}
+
+
