eb362635c6b3257a3685477fd5cab03ddf8e49e4
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-07-15 16:28:39 +0200

    Added support for motor power down/up

--- application/source/app_motor.c
+++ application/source/app_motor.c
@@ -1,10 +1,12 @@
 
 #include <xc.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #include "app_motor.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
+#include "app_timer.h"
 
 #define CONFIG_MSENSOR_GPIO_PORT        &GpioC
 #define CONFIG_MSENSOR_GPIO_PIN         0
@@ -13,19 +15,43 @@
 #define CONFIG_MDRIVE_GPIO_PORT         &GpioA
 #define CONFIG_MDRIVE_GPIO_PIN          4
 
+#define CONFIG_MDRIVE_POWER_PORT        &GpioA
+#define CONFIG_MDRIVE_POWER_PIN         10
+
+static bool isTimerElapsed;
+
+static void spinTimerHandler(void * arg)
+{
+    (void)arg;
+    isTimerElapsed = true;
+}
+
 void initMotorModule(void) {
     *(CONFIG_MSENSOR_GPIO_PORT)->tris  |=  (0x1u << CONFIG_MSENSOR_GPIO_PIN);
     *(CONFIG_MSENSOR_GPIO_PORT)->ansel |=  (0x1u << CONFIG_MSENSOR_GPIO_PIN);
     *(CONFIG_MDRIVE_GPIO_PORT)->tris   &= ~(0x1u << CONFIG_MDRIVE_GPIO_PIN);
     *(CONFIG_MDRIVE_GPIO_PORT)->clr     =  (0x1u << CONFIG_MDRIVE_GPIO_PIN);
+    *(CONFIG_MDRIVE_POWER_PORT)->tris  &= ~(0x1u << CONFIG_MDRIVE_POWER_PIN);
+    *(CONFIG_MDRIVE_POWER_PORT)->clr    =  (0x1u << CONFIG_MDRIVE_POWER_PIN);
 
     adcEnableChannel(CONFIG_MSENSOR_AD_CHANNEL, NULL);
 }
 
-void motorEnable(void) {
-    *(CONFIG_MDRIVE_GPIO_PORT)->set = (0x1u << CONFIG_MDRIVE_GPIO_PIN);
+void motorEnable(void)
+{
+    esVTimer delay;
+
+    isTimerElapsed = false;
+    *(CONFIG_MDRIVE_POWER_PORT)->set = (0x1u << CONFIG_MDRIVE_POWER_PIN);
+    esVTimerInit(&delay);
+    esVTimerStart(&delay, ES_VTMR_TIME_TO_TICK_MS(20), spinTimerHandler, NULL);
+
+    while (!isTimerElapsed);
+    *(CONFIG_MDRIVE_GPIO_PORT)->set  = (0x1u << CONFIG_MDRIVE_GPIO_PIN);
 }
 
-void motorDisable(void) {
-    *(CONFIG_MDRIVE_GPIO_PORT)->clr = (0x1u << CONFIG_MDRIVE_GPIO_PIN);
+void motorDisable(void)
+{
+    *(CONFIG_MDRIVE_POWER_PORT)->clr = (0x1u << CONFIG_MDRIVE_POWER_PIN);
+    *(CONFIG_MDRIVE_GPIO_PORT)->clr  = (0x1u << CONFIG_MDRIVE_GPIO_PIN);
 }
