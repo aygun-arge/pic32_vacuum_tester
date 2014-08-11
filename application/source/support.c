1768084b69dafc0a6a45ca79392b38aea028655f
Nenad Radulović <nenad.b.radulovic@gmail.com>
2014-08-11 09:02:01 +0200

    Added general exception handler

--- application/source/support.c
+++ application/source/support.c
@@ -1,11 +1,16 @@
 
 #include "base/debug.h"
 #include <string.h>
+#include <xc.h>
 
 static char function[100];
 static char expression[100];
 static char message[100];
 static char file[100];
+
+static volatile unsigned int _excep_code;
+static volatile unsigned int _excep_addr;
+static volatile unsigned int _excep_vaddr;
 
 void userAssert(
     const struct esDebugReport * dbgReport) {
@@ -16,4 +21,20 @@ void userAssert(
     strcpy(expression, dbgReport->expr);
     strcpy(message,    dbgReport->msg);
     strcpy(file,       dbgReport->modFile);
+}
+
+void _general_exception_handler(void)
+{
+    volatile          int code;
+    volatile          int intlev;
+
+    _excep_vaddr = _CP0_GET_BADVADDR();
+    _excep_code  = _CP0_GET_CAUSE();
+    _excep_code  = (_excep_code & 0x0000007C) >> 2;
+
+    /* Try to show what has happened. */
+    code =  (_excep_code & 0x000000FF) >> 2;
+    intlev =(_excep_code & 0x0000FF00) >> 10;
+
+    while(1);
 }
